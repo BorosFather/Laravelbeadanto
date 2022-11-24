bdbb08ec38cfc21090040de182f9d64cae4ffec5
BorosFather <boros.zoli024@gmail.com>
2022-11-24 10:53:43 +0100

    project befejezve

--- app/Http/Controllers/PerfumeController.php
+++ app/Http/Controllers/PerfumeController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Perfume;
 
 class PerfumeController extends Controller
 {
+
+    public function insertPerfunes(){
+        DB::table("perfumes")->insert([
+            ["name"=> "Axe", "type" => "Citrus", "price"=>225],
+            ["name"=> "Playboy", "type" => "Fás", "price"=>230],
+            ["name"=> "Diesel", "type" => "Virágos", "price"=>268],
+            ["name"=> "Iceberg", "type" => "Gyümölcsös", "price"=>269],
+            ["name"=> "Nike", "type" => "Büdös", "price"=>260],
+        ]);
+        echo "<h1>Adatok elmentve</h1>";
+    }
+
     public function getPerfumes() {
 
         $perfumes = Perfume::all();
-
-        return view( "/perfumes" );
+        return view( "/perfumes",[
+            "perfumes" => $perfumes
+        ] );
     }
 
     public function newPerfume() {
@@ -19,6 +34,12 @@ class PerfumeController extends Controller
     }
 
     public function storePerfume( Request $request ) {
+
+        $datas = $request->validate([
+            "name" => "required",
+            "type" => "required",
+            "price" => "required"
+        ]);
 
         $perfume = new Perfume;
 
@@ -41,6 +62,14 @@ class PerfumeController extends Controller
     }
 
     public function updatePerfume( Request $request ) {
+
+        $perfume = Perfume::where("id", $request->id)->first();
+        $perfume->name = $request->name;
+        $perfume->type = $request->type;
+        $perfume->price = $request->price;
+
+        $perfume->save();
+        return redirect("/perfumes");
 
     }
 
--- resources/views/new_perfume.blade.php
+++ resources/views/new_perfume.blade.php
@@ -1,6 +1,7 @@
 @extends( "layouts.master" )
 
 @section( "content" )
+<h1>Új Termék</h1>
 
 @if( $errors->any() )
     <ul>
